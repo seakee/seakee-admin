bcaccc407b6a13ec11ff938a243d5e3547f17dda
Seakee <seakee23@163.com>
2018-08-24 21:15:51 +0800

    刷新token时使用默认guard

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -15,6 +15,7 @@ class Admin
      */
     public function handle($request, Closure $next)
     {
+    	//为后台加载JWT eloquent配置文件，用于后台账号的登录和token刷新
 	    config(['jwt.user' => '\App\Models\Users\AdminUser']);
 	    config(['auth.providers.users.model' => \App\Models\Users\AdminUser::class]);
 
