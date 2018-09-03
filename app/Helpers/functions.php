5a598d3ebe42194abac4c78efdb50ce3f2cc90da
Seakee <seakee23@163.com>
2018-09-03 21:09:35 +0800

    优化注释

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -8,15 +8,15 @@
  */
 
 
-/**
- * 生成友好时间形式
- *
- * @param $from
- *
- * @return false|string
- */
 if (!function_exists('friendly_date')) {
-	function friendly_date($from)
+	/**
+	 * 生成友好时间形式
+	 *
+	 * @param $from
+	 *
+	 * @return false|string
+	 */
+	function friendly_date($from): string
 	{
 		static $now = NULL;
 		$now == NULL && $now = time();
@@ -57,44 +57,44 @@ if (!function_exists('friendly_date')) {
 	}
 }
 
-/**
- * 校验是否为中国手机号码
- *
- * @param string $number
- *
- * @return bool
- */
 if (!function_exists('is_phone_number')) {
+	/**
+	 * 校验是否为中国手机号码
+	 *
+	 * @param string $number
+	 *
+	 * @return bool
+	 */
 	function is_phone_number(string $number): bool
 	{
 		return preg_match('/^(\+?0?86\-?)?((13\d|14[57]|15[^4,\D]|17[3678]|18\d)\d{8}|170[059]\d{7})$/', $number);
 	}
 }
 
-/**
- * 校验是否为有效的用户名
- *
- * @param string $number
- *
- * @return bool
- */
 if (!function_exists('is_valid_name')) {
+	/**
+	 * 校验是否为有效的用户名
+	 *
+	 * @param string $name
+	 *
+	 * @return bool
+	 */
 	function is_valid_name(string $name): bool
 	{
 		return preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $name);
 	}
 }
 
-/**
- * 返回可读性更好的文件尺寸
- *
- * @param     $bytes
- * @param int $decimals
- *
- * @return string
- */
 if (!function_exists('human_file_size')){
-	function human_file_size($bytes, $decimals = 2)
+	/**
+	 * 返回可读性更好的文件尺寸
+	 *
+	 * @param     $bytes
+	 * @param int $decimals
+	 *
+	 * @return string
+	 */
+	function human_file_size($bytes, $decimals = 2): string
 	{
 		$size   = ['B', 'kB', 'MB', 'GB', 'TB', 'PB'];
 		$factor = floor((strlen($bytes) - 1) / 3);
@@ -103,16 +103,16 @@ if (!function_exists('human_file_size')){
 	}
 }
 
-/**
- * 获取指定key的输入值，并过滤空值项
- *
- * @param array                    $paramsKey
- * @param \Illuminate\Http\Request $request
- *
- * @return array
- */
 if (!function_exists('filter_request_params')) {
-	function filter_request_params(array $paramsKey, \Illuminate\Http\Request $request)
+	/**
+	 * 获取指定key的输入值，并过滤空值项
+	 *
+	 * @param array                    $paramsKey
+	 * @param \Illuminate\Http\Request $request
+	 *
+	 * @return array
+	 */
+	function filter_request_params(array $paramsKey, \Illuminate\Http\Request $request): array
 	{
 		foreach ($paramsKey as $key) {
 			if ($request->filled($key)) {
