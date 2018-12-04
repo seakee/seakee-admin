af503ac4daa79e0f233df7550fe4b8b7655eb8f6
Seakee <seakee23@163.com>
2018-12-04 15:30:37 +0800

    update

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,6 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('admin.name') }}</title>
+        <!-- Styles -->
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
     <div id="app"></div>
