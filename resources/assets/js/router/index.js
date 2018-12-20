fbaa2aac5ddff07c6bee5d720a1b184284a3c88e
Seakee <seakee23@163.com>
2018-12-20 18:12:50 +0800

    升级laravel-mix

--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -14,7 +14,7 @@ export const constantRouterMap = [
 
     {
         path     : '/login',
-        component: require('@/views/login')
+        component: () => import('@/views/login')
     }
 ];
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const path = require('path');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
+    .sass('resources/assets/sass/app.scss', 'public/css', { implementation: require('node-sass') })
     .webpackConfig({
         resolve: {
             extensions: ['.js', '.vue', '.json'],
