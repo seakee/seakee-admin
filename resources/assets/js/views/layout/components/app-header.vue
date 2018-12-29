ad6bc3c5eb31faa95460100b1d5c86c8700a9ef9
Seakee <seakee23@163.com>
2018-12-29 17:33:16 +0800

    清除无用代码

--- resources/assets/js/views/layout/components/app-header.vue
+++ resources/assets/js/views/layout/components/app-header.vue
@@ -4,28 +4,14 @@
             <h4>HaHaHaHaHaHa</h4>
         </div>
         <div class="header-nav">
-            <h4><i class="el-icon-setting"></i></h4>
+            <h4><i class="fas fa-tachometer-alt"></i></h4>
         </div>
     </el-header>
 </template>
 
 <script>
-    import ElRow from "element-ui/packages/row/src/row";
-
     export default {
-        components: {ElRow},
-        name      : "app-header",
-        data() {
-            return {
-                activeIndex: '1',
-                activeIndex2: '1'
-            };
-        },
-        methods   : {
-            handleSelect(key, keyPath) {
-                console.log(key, keyPath);
-            }
-        }
+        name: "app-header"
     }
 </script>
 
@@ -38,14 +24,16 @@
         background-color: #1e88e5 !important;
         z-index: 1003;
     }
-    .header-title{
-        min-height:100%;
+
+    .header-title {
+        min-height: 100%;
         min-width: 200px;
         background: rgba(0, 0, 0, 0.05);
         position: absolute;
         text-align: center;
     }
-    .header-nav{
+
+    .header-nav {
         padding-left: 200px;
         margin: 20px 20px;
     }
