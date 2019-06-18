9ffb2f96b5fea9ecfef222ddbe468cf57538e2b0
Seakee <seakee23@163.com>
2019-06-18 14:47:15 +0800

    边栏菜单渲染

--- resources/assets/js/views/layout/components/app-sidebar.vue
+++ resources/assets/js/views/layout/components/app-sidebar.vue
@@ -1,48 +1,39 @@
 <template>
     <aside class="el-aside">
         <el-menu default-active="" class="el-menu-vertical">
-            <el-submenu index="1">
-                <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span slot="title">导航一</span>
-                </template>
-                <el-menu-item-group>
-                    <span slot="title">分组一</span>
-                    <el-menu-item index="1-1">选项1</el-menu-item>
-                    <el-menu-item index="1-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                    <el-menu-item index="1-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="1-4">
-                    <span slot="title">选项4</span>
-                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+            <template v-for="(menu, parentIndex) in this.$store.getters.profile.menus">
+                <el-submenu :index="parentIndex">
+                    <template slot="title">
+                        <i class="fas" :class="menu.icon"></i>
+                        <span slot="title">{{ menu.name }}</span>
+                    </template>
+                    <template v-if="menu.nodes !== 'undefined'">
+                        <template v-for="(m, index) in menu.nodes">
+                            <el-menu-item :index="generateIndex(parentIndex,index)"><i class="fas" :class="m.icon"></i>{{ m.name }}</el-menu-item>
+                        </template>
+                    </template>
                 </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
-                <i class="el-icon-menu"></i>
-                <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3" disabled>
-                <i class="el-icon-document"></i>
-                <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-                <i class="el-icon-setting"></i>
-                <span slot="title">导航四</span>
-            </el-menu-item>
+            </template>
         </el-menu>
     </aside>
-
 </template>
 
 <script>
     export default {
-        name: "app-sidebar"
+        name   : "app-sidebar",
+        methods: {
+            generateIndex: function (parentIndex,index) {
+                return parentIndex+ "_" + index
+            }
+        }
     }
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
+    .el-menu-item i {
+        width: 20px;
+    }
+
     .el-aside {
         background-color: white;
         height: 100%;
@@ -54,7 +45,7 @@
         border-right: solid 1px #e6e6e6;
     }
 
-    .el-menu{
+    .el-menu {
         border-right: none;
     }
 
@@ -62,7 +53,8 @@
         min-width: 200px;
         min-height: 100%;
     }
-    .leftSlideHide{
-        min-width:64px;
+
+    .leftSlideHide {
+        min-width: 64px;
     }
 </style>
