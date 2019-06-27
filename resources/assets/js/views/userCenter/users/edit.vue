25e7d5f5e9bce0b8c052ccc0bbbb0165f0a04c53
Seakee <seakee23@163.com>
2019-06-27 15:27:46 +0800

    优化样式位置

--- resources/assets/js/views/userCenter/users/edit.vue
+++ resources/assets/js/views/userCenter/users/edit.vue
@@ -140,59 +140,11 @@
     .el-form {
         width: 400px;
     }
-    .el-page-header {
-        display: flex;
-        line-height: 24px;
-    }
-
-    .el-page-header__left {
-        display: flex;
-        cursor: pointer;
-        margin-right: 40px;
-        position: relative;
-    }
-    .el-page-header__left .el-icon-back {
-        font-size: 18px;
-        margin-right: 6px;
-        align-self: center;
-    }
-    .el-page-header__title {
-        font-size: 14px;
-        font-weight: 500;
-    }
-    .el-page-header__left:after {
-        content: "";
-        position: absolute;
-        width: 1px;
-        height: 16px;
-        right: -20px;
-        top: 50%;
-        transform: translateY(-50%);
-        background-color: #dcdfe6;
-    }
-    .el-page-header__content {
-        font-size: 18px;
-        color: #303133;
-    }
     .text {
         font-size: 14px;
     }
 
     .item {
         margin-bottom: 18px;
-    }
-
-    .clearfix:before,
-    .clearfix:after {
-        display: table;
-        content: "";
-    }
-
-    .clearfix:after {
-        clear: both
-    }
-
-    .box-card {
-        width: 100%;
     }
 </style>
