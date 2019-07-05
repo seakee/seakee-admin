1bdb020fb2452f5d10a9327564727b98e62f7aa6
Seakee <seakee23@163.com>
2019-07-05 17:20:50 +0800

    优化开发环境打包

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,17 @@
 let mix    = require('laravel-mix');
 const path = require('path');
 
+//生产环境开始版本控制
+if (mix.inProduction()) {
+    mix.version();
+}
+
+//非生产环境禁用懒加载
+if (!mix.inProduction()) {
+    mix.babelConfig({
+        plugins: ['dynamic-import-node'],
+    })
+}
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -13,7 +24,7 @@ const path = require('path');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css', { implementation: require('node-sass') })
+    .sass('resources/assets/sass/app.scss', 'public/css', {implementation: require('node-sass')})
     .webpackConfig({
         resolve: {
             extensions: ['.js', '.vue', '.json'],
@@ -21,7 +32,7 @@ mix.js('resources/assets/js/app.js', 'public/js')
                 '@': path.resolve(__dirname, 'resources/assets/js')
             }
         },
-        output: {
+        output : {
             // 未被列在 entry 且需要被打包出来的文件
             chunkFilename: 'js/[id].[hash].js'
         }
@@ -39,4 +50,4 @@ mix.js('resources/assets/js/app.js', 'public/js')
                 }
             }
         }
-    }).version();
+    });
