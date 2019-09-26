10da66a57100345901582e4553a4965ec719a5fb
seakee <seakee23@gmail.com>
2019-09-26 15:32:17 +0800

    路由

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,7 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        //
+        $this->mapAdminRoutes();
     }
 
     /**
@@ -69,5 +69,22 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * Define the "admin" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapAdminRoutes()
+    {
+        $config = config('admin.route');
+
+        Route::prefix($config['prefix'])
+            ->middleware($config['middleware'])
+            ->namespace($config['namespace'])
+            ->group(base_path('routes/admin.php'));
     }
 }
