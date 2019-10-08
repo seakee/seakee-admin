c8a6c7374e3e2885ce2bcdefeb312fb2deb2fede
seakee <seakee23@gmail.com>
2019-10-08 15:02:11 +0800

    页尾加入版权和版本信息

--- resources/assets/admin/views/layout/components/app-footer.vue
+++ resources/assets/admin/views/layout/components/app-footer.vue
@@ -1,20 +1,38 @@
 <template>
-    <el-footer height="40px">页尾</el-footer>
+    <el-footer height="40px">
+        <div class="version">
+            <b>Version</b> {{ version }}
+        </div>
+        <strong>Copyright &copy; {{ date.getFullYear() }} <a
+            href="https://github.com/seakee/seakee-admin">SeakeeAdmin</a>.</strong> All rights
+        reserved.
+    </el-footer>
 </template>
 
 <script>
     export default {
-        name: "app-footer"
+        name: "app-footer",
+        data() {
+            return {
+                date   : new Date(),
+                version: appConfig.version
+            }
+        }
     }
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
-.el-footer{
-    left: 200px;
-    padding: 10px 15px;
-    right: 0;
-    bottom: 0;
-    border-top: solid 1px #e6e6e6;
-    background: #ffffff;
-}
+    .version {
+        float: right;
+    }
+
+    .el-footer {
+        left: 200px;
+        padding: 10px 15px;
+        right: 0;
+        bottom: 0;
+        border-top: solid 1px #e6e6e6;
+        background: #ffffff;
+        font-size: 14px;
+    }
 </style>
