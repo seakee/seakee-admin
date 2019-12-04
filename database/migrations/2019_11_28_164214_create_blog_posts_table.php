ce66b2eb4436185e8716f3a32824089d1e29873f
seakee <seakee23@gmail.com>
2019-12-04 15:32:12 +0800

    Create BlogPosts Table Migration

---
+++ database/migrations/2019_11_28_164214_create_blog_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('author_id')->default(0)->unsigned();
+            $table->longText('content');
+            $table->longText('hide_content');
+            $table->text('title')->index('title');
+            $table->text('excerpt');
+            $table->tinyInteger('status')->default(1)->comment('1.已发布2.待发布3.草稿4.已删除');
+            $table->tinyInteger('comment_status')->default(1);
+            $table->text('name')->index('name');
+            $table->timestamp('posted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+}
