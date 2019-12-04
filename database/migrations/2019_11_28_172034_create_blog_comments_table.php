733d3697a78c1efed93631683dc9104d5e5dd669
seakee <seakee23@gmail.com>
2019-12-04 15:33:00 +0800

    Create BlogComments Table Migration

---
+++ database/migrations/2019_11_28_172034_create_blog_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('post_id')->index('post_id')->default(0)->unsigned();
+            $table->bigInteger('parent_id')->index('parent_id')->default(0)->unsigned();
+            $table->integer('author_id')->index('author_id')->default(0)->unsigned();
+            $table->string('author_ip');
+            $table->text('content');
+            $table->tinyInteger('approved')->default(1);
+            $table->string('agent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_comments');
+    }
+}
