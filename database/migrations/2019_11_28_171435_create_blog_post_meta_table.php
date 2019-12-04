84addc4b6a0582815825e5e5bf4be1b18180f52e
seakee <seakee23@gmail.com>
2019-12-04 15:32:34 +0800

    Create BlogPostMeta Table Migration

---
+++ database/migrations/2019_11_28_171435_create_blog_post_meta_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogPostMetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_post_meta', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('post_id')->default(0)->unsigned();
+            $table->string('name', 100)->index('name');
+            $table->text('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_post_meta');
+    }
+}
