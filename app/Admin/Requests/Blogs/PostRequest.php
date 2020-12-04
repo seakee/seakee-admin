eb92dec0dc9d058cf382d75dbb1f2b6f7d4ee888
seakee <seakee23@gmail.com>
2020-12-04 09:53:34 +0800

    PostRequest

---
+++ app/Admin/Requests/Blogs/PostRequest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * File: PostRequest.php
+ * Author: Seakee <seakee23@gmail.com>
+ * Homepage: https://seakee.top
+ * Date: 2019/12/10 1:56 下午
+ * Description:
+ */
+
+namespace Admin\Requests\Blogs;
+
+
+use Admin\Requests\Request;
+
+class PostRequest extends Request
+{
+
+}
